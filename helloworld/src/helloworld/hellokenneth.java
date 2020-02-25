5266d32fbc279025ebd05e06534740690cedf3c9
SLIIT <kennethchamarakc@gmail.com>
2020-02-24 22:22:50 -0800

    test

---
+++ helloworld/src/helloworld/hellokenneth.java
@@ -0,0 +1,9 @@
+package helloworld;
+
+public class hellokenneth {
+	public static void main(String args[]) {
+		System.out.println("Hello Kenneth");
+		System.out.println("Are you crazy man?");		
+	}
+
+}
